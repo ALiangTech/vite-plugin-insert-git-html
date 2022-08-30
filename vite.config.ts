bcf0efbcf9f2d7d3c2f07848051ec83863d9e712
hj <hanjun@yiye.ai>
2022-08-30 17:40:12 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { InsertGitToHtml } from './index.js';
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), InsertGitToHtml()]
+})
